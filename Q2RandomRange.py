8a02d8a9b448d95a0950ab777a389b89c9205018
Sam-Charalampidis <sam.charalampidis@stu.ocsb.ca>
2024-11-20 14:44:01 +0000

    Q2

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,19 @@
-# Create a program that accepts 2 numbers from the user. 
-# Your program will output a random number between the range given by the user.
+import random 
+random.seed()
+c = True
+while c == True:
+    try:
+        a = int(input("number 1 "))
+    except:
+        print("please use a number")
+        exit()
+    try:
+        b = int(input("number 2 "))
+    except:
+        print("please use a number")
+        exit()
+    c = False
+if a > b:
+    c = True
+    print("first number must be smaller!")
+print("Heres a random number ",random.randint(a,b))
